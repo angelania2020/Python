12d7926ccbe4b881ca6c987c9eefdeff6cde2d98
Angelina <73145144+angelania2020@users.noreply.github.com>
2020-11-17 19:46:17 +0200

    13.py, 14.py added

--- Basics/13.py
+++ Basics/13.py
@@ -80,10 +80,17 @@ while pos<len(a) and a[pos]>=X:
     pos+=1
 print(pos+1)
 
-#8
+#8!!!
 a=[int(i) for i in input('Enter elements: ').split()]
 num_distinct=1
 for i in range(0,len(a)-1):
     if a[i]!=a[i+1]:
         num_distinct+=1
 print(num_distinct)
+
+#9!!!
+a=[int(i) for i in input('Please enter elements of the list usiing spacebar: ').split()]
+a = [int(i) for i in input().split()]
+for i in range(1, len(a), 2):
+    a[i - 1], a[i] = a[i], a[i - 1]
+print(' '.join([str(i) for i in a]))
---
+++ Basics/14.py
@@ -0,0 +1,16 @@
+f=open('text.txt','r')
+f=open('text.txt','r',encoding=('utf8'))
+print(f)
+print('Name of the file: ',f.name)
+print('File contents: ',f.read())
+print('File is open in mode:',f.mode)
+
+print('File is closed: ',f.closed)
+f.close()
+print('And now closed: ',f.closed)
+
+with open('text.txt','w',encoding=('utf8')) as s:
+    s.write('I like Pyhton!')
+    
+    
+for print('File contents: ',s.read())
