c2195e6a8d679ddcfdf1d1aa34ae61a57ab6939d
Angelina <angelania@ya.ru>
2020-11-19 14:11:01 +0200

    Pole_Chudes_1, Pole_Chudes_2, voprosi.txt, too_tekstifailidega.txt added

---
+++ Basics/Pole_Chudes_1.py
@@ -0,0 +1,55 @@
+import random
+
+def viktorina(): #Задаем функцию для викторины
+    with open ('voprosi.txt','r',encoding='utf8') as file:
+        spisok_voprosov = file.read().splitlines()
+        strochka = random.randrange(0, len(spisok_voprosov))
+        vopros_otvet = str(spisok_voprosov[strochka])
+        for i in range(0, len(vopros_otvet)):
+            if vopros_otvet[i]==';':
+                vopros = vopros_otvet[0:i]
+                otvet = vopros_otvet[i+1:len(vopros_otvet)]
+        return otvet, vopros #Функция выдает ответ и вопрос
+
+otvet, vopros = viktorina() #Задаем переменные
+
+print(vopros)
+#Печатаем звездочки для скрытого слова:
+skritoje_slovo = []
+for bukva in range(0,len(otvet)):
+    skritoje_slovo.append('*')
+print(''.join(skritoje_slovo))
+
+popitki=0
+otgadannije_bukvi = '' #Сначала нет отгаданных букв
+
+while  popitki != 3:
+    #Пользоатель вводит свою догадку:
+    dogadka = input('Введите букву или слово: ').lower()
+    if dogadka == otvet:
+        print('Молодцы!');print();break
+    if dogadka in otvet and dogadka not in otgadannije_bukvi:
+        print ('Есть такая буква!')
+        otgadannije_bukvi += dogadka
+        podbor_bukv = '' #Для вывода отгаданных и скрытых букв
+        for bukva_otveta in otvet:
+            if bukva_otveta in otgadannije_bukvi:
+                podbor_bukv += bukva_otveta
+            else:
+                podbor_bukv += "*"
+        print("Угаданные буквы: ", podbor_bukv)
+    elif dogadka in otgadannije_bukvi:
+        print ('Вы уже называли эту букву!')
+        popitki += 1
+    else:
+        print('Такой буквы нет или неверное слово!')
+        popitki += 1
+    if podbor_bukv == otvet:
+       print('Вы правильно назвали все буквы!');print();break
+       
+if popitki == 3:
+    print('Вы не угадали слово! Правильный ответ: {}.'.format(otvet))
+    print()
+        
+if popitki == 3 or podbor_bukv == otvet or dogadka == otvet:
+    print('Конец игры!')
